86bb2dfd1f1d904a15dafc424e771c1ade84b2d9
Kylaaa <kyler.mulherin@gmail.com>
2019-12-02 00:07:17 -0800

    Update runTests.server.lua

--- runTests.server.lua
+++ runTests.server.lua
@@ -1,6 +1,6 @@
-local Promise = script.Parent
-local targetSource = Promise.src
-local TestEZ = require(Promise.packages.TestEZ)
+local Http = script.Parent
+local targetSource = Http.src
+local TestEZ = require(Http.packages.TestEZ)
 local Reporter = TestEZ.Reporters.TextReporterQuiet
 
 TestEZ.TestBootstrap:run(targetSource:GetChildren(), Reporter)
