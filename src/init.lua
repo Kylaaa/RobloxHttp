cdc4645c3be9960b1d5a25aca401c12acc555d8c
Kylaaa <kyler.mulherin@gmail.com>
2023-04-24 15:20:22 -0400

    Remove Dev-Dependencies

--- src/init.lua
+++ src/init.lua
@@ -3,10 +3,10 @@
 	for executing HTTP requests.
 
 	Usage :
-	local packages = script.Parent.Parent.Parent.packages
-	local Http = require(packages.Http).new()
+	local Packages = game.ReplicatedStorage.Packages
+	local Http = require(Packages.Http).new()
 
-	local request = HttpGET("https://www.someDomain.com/json")
+	local request = Http.GET("https://www.someDomain.com/json")
 	request:andThen(function(response)
 		-- handle the successful request's response
 		print(tostring(response))
